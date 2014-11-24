4996cbdcdd5d1929e42f5f97e1188e5034b0d62c
Renato Luna <renatoluna@gmail.com>
2014-11-24 07:55:08 -0200

    Melhorando esquema de Classes

--- class.js
+++ class.js
@@ -1,16 +1,43 @@
-var $class = function() {
-    this.method1 = function() {
-        console.log(1);
-    };
-
-    this.factory = function(childClass) {
-        childClass.prototype = this;    
-        return new childClass(this);
-    };
+Function.prototype.extend = function (body) {
+    var constructor = function () {};
+    var prototype = constructor.prototype = new this;
+    body.call(prototype, this.prototype);
+    return constructor;
 };
 
-var myClass = function() {
-    return new $class().factory(function(parent) {
-
-    });
+Function.prototype.create = function () {
+    var instance = new this;
+    instance.constructor.apply(instance, arguments);
+    return instance;
 };
+
+var Person = Object.extend(function () {
+    this.constructor = function (isDancing) {
+        this.dancing = isDancing;
+    };
+
+    this.dance = function () {
+        return this.dancing;
+    };
+});
+
+var Ninja = Person.extend(function (base) {
+    this.constructor = function () {
+        base.constructor.call(this, false);
+    };
+
+    this.swingSword = function () {
+        return true;
+    };
+});
+
+var p = Person.create(true);
+p.dance(); // => true
+
+var n = Ninja.create();
+n.dance(); // => false
+n.swingSword(); // => true
+
+// Should all be true
+p instanceof Person && p instanceof Object &&
+n instanceof Ninja && n instanceof Person && n instanceof Object
