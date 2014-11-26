2d506ecdadcc9fd339fc224abb134add63e73bc7
Renato Lopes de Luna Freire <renatoluna@gmail.com>
2014-11-26 14:56:46 -0200

    Criando gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('javascript', function() {
+  gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
