562c759f5ffeda591bf7e92e128bb5e13b12a84f
Renato Lopes de Luna Freire <cad_rfreire@uolinc.com>
2015-01-12 16:55:25 -0200

    Adicionando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
   gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('cabrito.js'))
+    .pipe(uglify())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
 });
