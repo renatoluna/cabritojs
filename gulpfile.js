d9728dea0261ad5c6e84d03ef4a8a2beccb6161a
Renato Lopes de Luna Freire <cad_rfreire@uolinc.com>
2015-01-26 10:39:28 -0200

    Reestruturando o projeto

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,26 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
-gulp.task('dist', function() {
-  gulp.src('src/**/*.js')
-    .pipe(sourcemaps.init())
+gulp.task('build', function() {
+    gulp.src([
+        'src/cabrito.js', 
+        'src/modules/**.js'
+    ])
     .pipe(concat('cabrito.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('min', function() {
+    gulp.src([
+        'src/cabrito.js', 
+        'src/modules/**.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('cabrito.min.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['dist']);
+gulp.task('default', ['build']);
+gulp.task('dist', ['build', 'min']);
